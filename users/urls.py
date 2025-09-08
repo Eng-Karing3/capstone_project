ddbee2ec669a1e45d9af3b181f844435fb0f8ebc
Karinge Muniu <karingemuniu@gmail.com>
2025-09-08 21:48:26 +0300

    Fixed up errors on settings.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
+
 from .views import RoleListCreateView, ProfileListCreateView, ProfileDetailView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="profiles/")),
     path('roles/', RoleListCreateView.as_view(), name='role-list-create'),
     path('profiles/', ProfileListCreateView.as_view(), name='profile-list-create'),
     path('profiles/<int:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
